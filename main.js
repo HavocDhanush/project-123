ae530eea5e55fc42e3cb8fade5d3838092461ab5
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2023-11-13 15:46:58 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(400,400);
@@ -7,25 +12,35 @@ function setup(){
     canvas.position(430,130);
 
     poseNet = ml5.poseNet(video,modelDone);
-    poseNet.on('pose',gotposes);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     background("#5196e3");
+    textSize(difference);
+    fill ('#F90093');
+    text("Dhanush" , 50, 200);
+    
+    
+        
 }
 
 function modelDone(){
     console.log("PoseNet Is Initialized And Loaded");
 }
 
-function gotposes(results,error){
+function gotPoses(results,error){
     if(error){
         console.error(error);
     }
     if(results.length > 0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
 
         console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
         console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
     }
 }
+
